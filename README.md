9ceb090dd5c641eda554a2c329f4cd25064e64e4
tanakakjs <oneok0813@gmail.com>
2020-06-23 21:01:04 +0900

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# uiux-handson
+
+> uiux handson project
+
+## Development
+
+```bash
+yarn dev
+yarn build
+```
+
+For more details, please head VuePress's [documentation](https://v1.vuepress.vuejs.org/).
+
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,89 @@
+const { description } = require("../../package");
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: "Vuepress Docs Boilerplate",
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: "",
+    editLinks: false,
+    docsDir: "",
+    editLinkText: "",
+    lastUpdated: false,
+    nav: [
+      {
+        text: "Handson",
+        link: "/Handson/",
+      },
+      {
+        text: "Config",
+        link: "/config/",
+      },
+      {
+        text: "VuePress",
+        link: "https://v1.vuepress.vuejs.org",
+      },
+    ],
+    sidebar: {
+      "/guide/": [
+        {
+          title: "Guide",
+          collapsable: false,
+          children: ["", "using-vue"],
+        },
+      ],
+      "/": [
+        {
+          title: "UIUXハンズオンについて",
+          collapsable: true,
+          children: ["/handson/", "/handson/test"],
+        },
+        {
+          title: "初級編",
+          collapsable: true,
+          children: ["/beginner/start"],
+        },
+        {
+          title: "中級編",
+          collapsable: true,
+          children: ["/intermediate/start", "/intermediate/vue-introduction"],
+        },
+        {
+          title: "上級編",
+          collapsable: true,
+          children: ["/advanced/start"],
+        },
+      ],
+    },
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+};
